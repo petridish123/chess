0db7893895fb154693bbc633c12c4effc5399874
Peter <peter.c.gordon@gmail.com>
2025-01-10 16:33:21 -0700

    Beginning the king moves calculator

--- shared/src/main/java/chess/PieceMoveCalculator.java
+++ shared/src/main/java/chess/PieceMoveCalculator.java
@@ -1,4 +1,23 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class PieceMoveCalculator {
+
+    private final ChessBoard board;
+    private final ChessPosition position;
+    private final ChessPiece piece;
+
+    public PieceMoveCalculator(ChessBoard board, ChessPosition position) {
+        this.board = board;
+        this.position = position;
+        this.piece = board.getPiece(position);
+    }
+
+    public Collection<ChessMove> KingMovesCalculator(){
+        return new ArrayList<ChessMove>();
+    }
+
+
 }
