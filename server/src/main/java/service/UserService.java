965dbafbe5bbb28ea879f4599b88a2022af6bc7a
Peter G <peter.c.gordon@gmail.com>
2025-03-01 11:48:21 -0700

    User service endpoints complete (I think)

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -8,8 +8,8 @@ import java.util.UUID;
  * User Service endpoints
  *
  * register ✅
- * login
- * logout
+ * login    ✅
+ * logout   ✅
  *
  * */
 
@@ -34,6 +34,24 @@ public class UserService {
         return auth;
     }
 
+    AuthData loginUser(UserData user) throws DataAccessException { // receives only username and password
+        /**
+         * @user takes in only password and username to check the DB
+         */
+        UserData potentialUser = userDAO.getUserData(user.username()); // throws DataAccessException "no user"
+        if (!potentialUser.password().equals(user.password())) { // Finds the user based on username and then checks password
+            throw new DataAccessException("Wrong password");
+        }
+        var authToken = UUID.randomUUID().toString();
+        var auth = new AuthData(authToken, user.username());
+        authTokenDAO.insertAuthData(auth); // throws exception "already taken"
+        return auth;
+    }
+
+    void logoutUser(AuthData authToken) throws DataAccessException {
+        var token = authTokenDAO.getAuthData(authToken.authToken()); // does not exist
+        authTokenDAO.deleteAuthData(token.authToken());
+    }
     
 
 }
