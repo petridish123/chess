fd79b4b08caf991e39d802ce698bc1e0a86ed67d
Peter G <peter.c.gordon@gmail.com>
2025-03-07 13:44:47 -0700

    Getting started on phase 4

---
+++ server/src/main/java/dataaccess/MySqlGameDAO.java
@@ -0,0 +1,85 @@
+package dataaccess;
+
+import model.GameData;
+
+import java.sql.*;
+import java.util.HashSet;
+import java.util.Properties;
+
+public class MySqlGameDAO implements GameDataAccess{
+    private static final String DATABASE_NAME;
+    private static final String USER;
+    private static final String PASSWORD;
+    private static final String CONNECTION_URL;
+    static {
+        try {
+            try (var propStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("db.properties")) {
+                if (propStream == null) {
+                    throw new Exception("Unable to load db.properties");
+                }
+                Properties props = new Properties();
+                props.load(propStream);
+                DATABASE_NAME = props.getProperty("db.name");
+                USER = props.getProperty("db.user");
+                PASSWORD = props.getProperty("db.password");
+
+                var host = props.getProperty("db.host");
+                var port = Integer.parseInt(props.getProperty("db.port"));
+                CONNECTION_URL = String.format("jdbc:mysql://%s:%d", host, port);
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("unable to process db.properties. " + ex.getMessage());
+        }
+    }
+    public MySqlGameDAO() throws DataAccessException {
+        try {
+            var statement = "CREATE DATABASE IF NOT EXISTS " + DATABASE_NAME;
+            var conn = DriverManager.getConnection(CONNECTION_URL, USER, PASSWORD);
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    static Connection getConnection() throws DataAccessException {
+        try {
+            var conn = DriverManager.getConnection(CONNECTION_URL, USER, PASSWORD);
+            conn.setCatalog(DATABASE_NAME);
+            return conn;
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    @Override
+    public HashSet<GameData> listGames() {
+        return null;
+    }
+
+    @Override
+    public GameData getGame(int id) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void createGame(GameData game) throws DataAccessException {
+
+    }
+
+    @Override
+    public void updateGame(GameData game) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean getGameByname(String name) throws DataAccessException {
+        return false;
+    }
+}
