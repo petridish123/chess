30f2aa47a1401d3f64b8a85933bc38d3f1f6b5a6
Peter G <peter.c.gordon@gmail.com>
2025-03-22 14:09:06 -0600

    Began writing the REPLs for phase 5

---
+++ client/src/main/java/repls/preLoginREPL.java
@@ -0,0 +1,25 @@
+package repls;
+
+public class preLoginREPL {
+
+    public void run() {
+        System.out.println("\uD83D\uDC36 Welcome to the pet store. Sign in to start.");
+        System.out.print(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                System.out.print(BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+}
