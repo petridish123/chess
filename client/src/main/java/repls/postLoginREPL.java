d3451fb0b014b221138b239d49d406331ee60818
Peter G <peter.c.gordon@gmail.com>
2025-03-23 12:38:02 -0600

    finished pre login repl

---
+++ client/src/main/java/repls/postLoginREPL.java
@@ -0,0 +1,14 @@
+package repls;
+
+import server.ServerFacade;
+
+public class postLoginREPL {
+    ServerFacade facade;
+    public postLoginREPL(ServerFacade facade) {
+        this.facade = facade;
+    }
+
+    void run(){
+
+    }
+}
--- client/src/main/java/repls/preLoginREPL.java
+++ client/src/main/java/repls/preLoginREPL.java
@@ -1,25 +1,88 @@
 package repls;
 
+import exception.ResponseException;
+import server.ServerFacade;
+
+import java.util.Scanner;
+import static ui.EscapeSequences.*;
+import static java.lang.System.out;
+
 public class preLoginREPL {
+    ServerFacade facade;
+    postLoginREPL postLogin;
+    public preLoginREPL(ServerFacade facade) {
+        this.facade = facade;
+        this.postLogin = new postLoginREPL(facade);
+    }
 
     public void run() {
-        System.out.println("\uD83D\uDC36 Welcome to the pet store. Sign in to start.");
-        System.out.print(client.help());
+        out.println(SET_BG_COLOR_RED + "Welcome to chess! \nPlease read the help to log in and get playing!" + RESET_BG_COLOR);
+        helpPrint();
+        boolean login = false;
+        while (!login) {
+            String[] input = getUserInput();
+            switch (input[0]) {
+                case "register":
+                    if (input.length <4){
+                        out.println("You didn't enter enough arguments!");
+                        registerPrint();
+                        break;
+                    }
+                    try{
+                        facade.register(input[1], input[2], input[3]);
+                        login = true;
+                        break;
+                    }catch (ResponseException e){
+                        out.println("User already exists!");
+                    }
 
-        Scanner scanner = new Scanner(System.in);
-        var result = "";
-        while (!result.equals("quit")) {
-            printPrompt();
-            String line = scanner.nextLine();
-
-            try {
-                result = client.eval(line);
-                System.out.print(BLUE + result);
-            } catch (Throwable e) {
-                var msg = e.toString();
-                System.out.print(msg);
+                case "login":
+                    if (input.length < 3) {
+                        out.println("You didn't enter enough arguments!");
+                        loginPrint();
+                        break;
+                    }
+                    try {
+                        facade.login(input[1],input[2]);
+                        login = true;
+                        break;
+                    } catch (ResponseException e) {
+                        out.println("Please use a valid username and password!");
+                        loginPrint();
+                    }
+                    break;
+                case "quit":
+                    out.println("Goodbye!");
+                    return;
+                default:
+                    helpPrint();
+                    break;
             }
         }
-        System.out.println();
+        out.println();
+        postLogin.run();
+    }
+
+
+
+    private String[] getUserInput() {
+        out.print("\n[LOGGED OUT] >>> ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine().split(" ");
+    }
+
+    private void helpPrint() {
+        registerPrint();
+        loginPrint();
+        out.println("quit : exit");
+        out.println("help : display help");
+    }
+
+    private void registerPrint() {
+        out.println("register <USERNAME> <PASSWORD> <EMAIL> - create a new user");
+    }
+
+    private void loginPrint() {
+        out.println("login <USERNAME> <PASSWORD> - login to an existing user");
     }
 }
