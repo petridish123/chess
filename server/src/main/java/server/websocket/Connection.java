82cf959d2ba746ca2606b6ccf2ccd398b84b8e6e
Peter G <peter.c.gordon@gmail.com>
2025-03-27 15:43:06 -0600

    Finished the connection manager

--- server/src/main/java/server/websocket/Connection.java
+++ server/src/main/java/server/websocket/Connection.java
@@ -1,5 +1,6 @@
 package server.websocket;
 
+import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
 
 import java.io.IOException;
@@ -13,7 +14,8 @@ public class Connection {
         this.session = session;
     }
 
-    public void send(String msg) throws IOException { // May need to modify in order to send the right thing
-        session.getRemote().sendString(msg);
+    public void send(String msg) throws IOException {// May need to modify in order to send the right thing
+        var new_msg = new Gson().toJson(msg);
+        session.getRemote().sendString(new_msg);
     }
 }
--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,6 +1,9 @@
 package server.websocket;
 import org.eclipse.jetty.websocket.api.Session;
 import websocket.*;
+import websocket.messages.Notification;
+import websocket.messages.ServerMessage;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
@@ -9,6 +12,7 @@ public class ConnectionManager {
 
     /*
     TODO: maybe add a group / gameID so that I keep multiple hashmaps Or the hashmap is of hashmaps!!
+    TODO: Make multiple connection managers 1: for each game!
      */
     public void add(String username, Session session) {
         var connection = new Connection(username, session);
@@ -22,5 +26,22 @@ public class ConnectionManager {
     /*
     TODO: I need to create a function that broadcasts to a specific group
      */
+    public void broadcastNotification(String excludeVisitorName, ServerMessage notification) throws IOException {
+        var removeList = new ArrayList<Connection>();
+        for (var c : connections.values()) {
+            if (c.session.isOpen()) {
+                if (!c.username.equals(excludeVisitorName)) {
+                    c.send(notification.toString());
+                }
+            } else {
+                removeList.add(c);
+            }
+        }
+
+        // Clean up any connections that were left open.
+        for (var c : removeList) {
+            connections.remove(c.username);
+        }
+    }
 
 }
