c98343e587cc3e0994ff094ce6f98a9e38e4382a
Peter G <peter.c.gordon@gmail.com>
2025-03-26 21:28:38 -0600

    Update Connection.java

--- server/src/main/java/server/websocket/Connection.java
+++ server/src/main/java/server/websocket/Connection.java
@@ -5,15 +5,15 @@ import org.eclipse.jetty.websocket.api.Session;
 import java.io.IOException;
 
 public class Connection {
-    public String visitorName;
+    public String username;
     public Session session;
 
-    public Connection(String visitorName, Session session) {
-        this.visitorName = visitorName;
+    public Connection(String username, Session session) {
+        this.username = username;
         this.session = session;
     }
 
-    public void send(String msg) throws IOException {
+    public void send(String msg) throws IOException { // May need to modify in order to send the right thing
         session.getRemote().sendString(msg);
     }
 }
