671abba245131b6474fce2c98b93b372bd795e71
Peter G <peter.c.gordon@gmail.com>
2025-04-04 16:41:05 -0600

    IMA FLIP N TURN IN PLACE GABAGOOEY

--- shared/src/main/java/facade/websocketWriter.java
+++ shared/src/main/java/facade/websocketWriter.java
@@ -1,4 +1,43 @@
 package facade;
 
-public class websocketWriter {
+import javax.websocket.*;
+
+
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class websocketWriter extends Endpoint {
+
+    Session session;
+
+    public websocketWriter(String serverDomain) throws Exception {
+        try {
+            URI uri = new URI("ws://" + serverDomain + "/ws");
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, uri);
+
+            //set message handler
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    handleMessage(message);
+                }
+            });
+
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new Exception();
+        }
+
+    }
+
+
+    private void handleMessage(String message) {
+    }
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
 }
