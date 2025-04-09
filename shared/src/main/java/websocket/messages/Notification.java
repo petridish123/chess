d3db3effa91844658c7d11992724c44eddc86058
Peter G <peter.c.gordon@gmail.com>
2025-04-08 22:46:07 -0600

    Update Notification.java

--- shared/src/main/java/websocket/messages/Notification.java
+++ shared/src/main/java/websocket/messages/Notification.java
@@ -1,13 +1,13 @@
 package websocket.messages;
 
 public class Notification extends ServerMessage {
-    String notification;
+    String message;
     public Notification(String notification) {
         super(ServerMessageType.NOTIFICATION);
-        this.notification = notification;
+        this.message = notification;
     }
     public String getNotification() {
-        return notification;
+        return message;
     }
 
 
